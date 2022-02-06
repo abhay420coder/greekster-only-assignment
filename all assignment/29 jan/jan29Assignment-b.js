9fa2b44f9a7d9601df72c9bbe0f5a5f88435b8b2
abhay420coder <96329957+abhay420coder@users.noreply.github.com>
2022-02-06 19:41:44 +0530

    Add files via upload

--- all assignment/29 jan/jan29Assignment-b.js
+++ all assignment/29 jan/jan29Assignment-b.js
@@ -1,6 +1,15 @@
 // user will give a string ,r eturn the count of vowels
 // aayush == output = 3
 // aAyush == output = 3
-var nam = prompt('enter name')
+var nam = prompt("enter name")
 var sam = nam.split('');
-console.log(sam)
+var count = 0
+for (let m=0 ; m<sam.length ; m++)
+{
+    if (sam[m]=="a" || sam[m]=="A" || sam[m]=="e" || sam[m]=="E" || sam[m]=="i" || sam[m]=="I" || sam[m]=="o" || sam[m]=="O" || sam[m]=="u" || sam[m]=="U")
+    {
+        count=count+1
+    }
+}
+console.log(count)
+
